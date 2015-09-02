26b0139dac7e0e404d8db9396787aafc9099bc42
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-09-03 02:59:45 +0700

    first assignment

---
+++ firstAssignment/README.md
@@ -0,0 +1,33 @@
+#Reimplement Haskell function
+
+##Rules!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+1. **Don't use google to find how a haskell function works**
+2. for 1.a and 1.b do not use any other function except : pred, succ, ++, :, | (guard), partern matching
+3. for 1.b you can use 1.a function to reimplement 1.b
+
+## Small help
+
+don't forget to add "import Data.List" to your *.hs file, or ghci/winghci (we recommend to put it in *.hs file)
+
+for showing runtime and type system evertime you run something
+:set +s
+:set +t
+
+Use template.hs in firstAssignment, to help tutors review your code
+
+## 1.a List of function to reimplement
+
+null, take, drop, fst, snd, map, filter, delete, deleteAll, foldl, foldl1, zip, zipWith, nth,
+sort, scanl, scanl1, elem, notElem, head, length, reverse, last, tail, init, max, min, concat, intersperse, intercalate, and, or, zip3, sum, product, words, lines, unlines, unwords, takeWhile, dropWhile, concatMap, all, any, insert, zipWith3
+
+-- nth is (!!)
+
+## 1.b List of function to reimplement
+
+you can use your other function from 1.a to solve this :
+
+nub, sort, minimum, maximum, inits, tails, union, intersect, group, splitAt, partition, replicate
+
+If you want to be teacher's pet
+reimplement subsequences, permutations
---
+++ firstAssignment/template.hs
@@ -0,0 +1,242 @@
+-- First Assignment
+-- Reimplement Haskell function
+-- DON'T USE GOOGLE
+module Template where
+
+import Data.List
+
+-- 1.a
+
+null' x = x
+
+--pembatas
+
+take' x = x
+
+--pembatas
+
+drop' x = x
+
+--pembatas
+
+fst' x = x
+
+--pembatas
+
+snd' x = x
+
+--pembatas
+
+map' x = x
+
+--pembatas
+
+filter' x = x
+
+--pembatas
+
+delete' x = x
+
+--pembatas
+
+deleteAll' x = x
+
+--pembatas
+
+foldl' x = x
+
+--pembatas
+
+foldl1' x = x
+
+--pembatas
+
+zip' x = x
+
+--pembatas
+
+zipWith' x = x
+
+--pembatas
+
+nth' x = x
+
+--pembatas
+
+sort' x = x
+
+--pembatas
+
+scanl' x = x
+
+--pembatas
+
+scanl1' x = x
+
+--pembatas
+
+elem' x = x
+
+--pembatas
+
+notElem' x = x
+
+--pembatas
+
+head' x = x
+
+--pembatas
+
+length' x = x
+
+--pembatas
+
+reverse' x = x
+
+--pembatas
+
+last' x = x
+
+--pembatas
+
+tail' x = x
+
+--pembatas
+
+init' x = x
+
+--pembatas
+
+max' x = x
+
+--pembatas
+
+min' x = x
+
+--pembatas
+
+concat' x = x
+
+--pembatas
+
+intersperse' x = x
+
+--pembatas
+
+intercalate' x = x
+
+--pembatas
+
+and' x = x
+
+--pembatas
+
+or' x = x
+
+--pembatas
+
+zip3' x = x
+
+--pembatas
+
+sum' x = x
+
+--pembatas
+
+product' x = x
+
+--pembatas
+
+words' x = x
+
+--pembatas
+
+lines' x = x
+
+--pembatas
+
+unlines' x = x
+
+--pembatas
+
+unwords' x = x
+
+--pembatas
+
+takeWhile' x = x
+
+--pembatas
+
+dropWhile' x = x
+
+--pembatas
+
+concatMap' x = x
+
+--pembatas
+
+all' x = x
+
+--pembatas
+
+any' x = x
+
+--pembatas
+
+insert' x = x
+
+--pembatas
+
+zipWith3' x = x
+
+--pembatas
+
+-- 1.b
+
+nub' x = x
+
+--pembatas
+
+sort' x = x
+
+--pembatas
+
+minimum' x = x
+
+--pembatas
+
+maximum' x = x
+
+--pembatas
+
+inits' x = x
+
+--pembatas
+
+tails' x = x
+
+--pembatas
+
+union' x = x
+
+--pembatas
+
+intersect' x = x
+
+--pembatas
+
+group' x = x
+
+--pembatas
+
+splitAt' x = x
+
+--pembatas
+
+partition' x = x
+
+--pembatas
+
+replicate' x = x
+
+--pembatas
